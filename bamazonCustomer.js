528f5d91ab187a60e845748380323e0cbffa192d
Willie Smalls <williesmalls@Willies-MacBook-Air.local>
2018-05-17 16:15:23 -0400

    put inquirer and SQL together

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,7 +1,6 @@
 //npm packages
 var mysql = require("mysql");
-// var inquirer = require('inquirer');
-var inquirer = require('inquirer-promise');
+var inquirer = require('inquirer');
 
 //connection string
 var connection = mysql.createConnection({
@@ -21,77 +20,81 @@ connection.connect(function(error){
 
 });
 
+//questions for bamazon users
+var questions = [
+ 
+    {
+      type: 'input',
+      name: 'itemId',
+      message: 'What is item id interests you?',
+      validate: function(value) {
+        var valid = !isNaN(parseFloat(value));
+        return valid || 'Please enter a number';
+      },
+      filter: Number
+    },
+    {
+      type: 'input',
+      name: 'numUnits',
+      message: 'How many units?',
+      validate: function(value) {
+        var valid = !isNaN(parseFloat(value));
+        return valid || 'Please enter a number';
+      },
+      filter: Number
+    }
+  ];
+
+
 //pull the products.  
 function displayProducts(){
     connection.query("select item_id, product_name, department_name, price, stock_quantity from products", function (error, response){
         if(error) throw error;
-        
+
         console.log(response);
-        connection.end();
+        //connection.end();
     });
 }
 
-//display the products
-//displayProducts();
-
-
-var units = 2;
-var item_id = 1; 
 
 //Primary SQL queries
 var sqlQuery = "SELECT product_name, price, stock_quantity FROM products WHERE item_id = ?"
 var sqlUpdate = "UPDATE products SET stock_quantity = ? WHERE item_id = ?"
 
-function currentUnits(){
+
+function currentUnits(item_id, units){
+
+//SQL query to pull data 
+connection.query(sqlQuery, [item_id], function (error, response){
+    var current_units;
+    var price;
+    var remaining_units; 
+    var product_name; 
+
+    if(error) throw error;
     
-    //SQL query to pull data 
-    connection.query(sqlQuery, [item_id], function (error, response){
-        var current_units;
-        var price;
-        var remaining_units; 
-        var product_name; 
+    current_units = response[0].stock_quantity;
+    price = response[0].price;
 
-        if(error) throw error;
-        
-        current_units = response[0].stock_quantity;
-        price = response[0].price;
-
-        if (current_units >= units){
-            remaining_units = current_units - units; 
-            connection.query(sqlUpdate, [remaining_units, item_id], function(error, response){
-                if(error) throw error;
-                connection.end();
-            });
-            console.log('The Price For ' + units + ' of ' + product_name + ' is ' + '$' + price * units);
-        } 
-        else{
-            console.log('Insufficient quantity!');
-            connection.end();
-        } 
-
-    });
+    if (current_units >= units){
+        remaining_units = current_units - units; 
+        connection.query(sqlUpdate, [remaining_units, item_id], function(error, response){
+            if(error) throw error;
+        });
+        console.log('The Price For ' + units + ' of ' + response[0].product_name + ' is ' + '$' + price * units);
+    } 
+    else{
+        console.log('Insufficient quantity!');
+    } 
+    connection.end();
+});
 }
+  
+//display the products
+displayProducts();
 
-//currentUnits();
-connection.end();
-
-var questions = [
-    {
-        type: 'input',
-        name: 'itemId',
-        message: 'What is item id interests you?'
-    },
-    {
-        type: 'input',
-        name: 'numUnits',
-        message: 'How many units?'
-    }
-];
-var itemId; 
-var numUnits; 
-inquirer.prompt(questions).then(responses => 
-    console.log(responses.itemId, responses.numUnits);
-    console.log(itemid, numUnits);
-
-);
+inquirer.prompt(questions).then(answers => {
+//run function for inquire and SQL
+currentUnits(answers.itemId, answers.numUnits);
+});
 
